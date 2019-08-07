6fa2b275cfcf76ade2c5e2575d6c76a17aba4309
Michael Burgin <realripley00@gmail.com>
2019-08-07 13:39:31 -0400

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 /**
  * Simple safari detection based on user agent test
  */
-export const isSafari = () => /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export const isSafari = () => /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
 export const isJsons = ((array) => Array.isArray(array) && array.every(
  row => (typeof row === 'object' && !(row instanceof Array))
